b8c49d369e3654bd8c041bf45151600ca3286cdd
Jameson Lee <jameson@uber.com>
2015-04-17 14:25:33 -0700

    bump 0.3.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='clustoclient',
-      version='0.3.2',
+      version='0.3.3',
       py_modules=['clustohttp'],
       scripts=['clusto-template', 'clusto-get-from-pools'],
       install_requires=['jinja2', 'IPy'])
